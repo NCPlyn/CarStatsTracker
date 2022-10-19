c9b0302d226cb3fa00683f264f1df288d32f4fb1
NCPlyn <jakobabc@seznam.cz>
2022-10-20 01:22:04 +0200

    Minulé ESP bylo vadné, s novým velké pokroky

--- beta/beta.ino
+++ beta/beta.ino
@@ -13,9 +13,20 @@
 #define SPIFFS LITTLEFS
 #include <LITTLEFS.h>
 #include "loraCreds.h"
+#include "FS.h"
+#include "SD.h"
 
-#define LORAWAN_APP_DATA_BUFF_SIZE 50  /**< Size of the data to be transmitted. */
-#define JOINREQ_NBTRIALS 3			   /**< Number of trials for the join request. */
+#define LORALED 26
+
+#define SD_CS 15
+#define SPI_SCK 14
+#define SPI_MISO 27
+#define SPI_MOSI 13
+SPIClass SPISD(HSPI);
+//Also to properly work, add "VSPI" to the () in 'SX126x-Arduino\src\boards\mcu\spi_board.cpp' on line 5
+
+#define LORAWAN_APP_DATA_BUFF_SIZE 50 // Size of the data to be transmitted
+#define JOINREQ_NBTRIALS 3  // Number of trials for the join request
 hw_config hwConfig;
 // Foward declaration
 static void lorawan_has_joined_handler(void);
@@ -23,11 +34,11 @@ static void lorawan_rx_handler(lmh_app_data_t *app_data);
 static void lorawan_confirm_class_handler(DeviceClass_t Class);
 static void lorawan_join_failed_handler(void);
 static void send_lora_frame(void);
-static uint8_t m_lora_app_data_buffer[LORAWAN_APP_DATA_BUFF_SIZE];			  ///< Lora user application data buffer.
-static lmh_app_data_t m_lora_app_data = {m_lora_app_data_buffer, 0, 0, 0, 0}; ///< Lora user application data structure.
+static uint8_t m_lora_app_data_buffer[LORAWAN_APP_DATA_BUFF_SIZE]; // Lora user application data buffer.
+static lmh_app_data_t m_lora_app_data = {m_lora_app_data_buffer, 0, 0, 0, 0}; // Lora user application data structure.
 static lmh_param_t lora_param_init = {LORAWAN_ADR_ON, DR_3, LORAWAN_PUBLIC_NETWORK, JOINREQ_NBTRIALS, LORAWAN_DEFAULT_TX_POWER, LORAWAN_DUTYCYCLE_OFF}; //Structure containing LoRaWan parameters, needed for lmh_init()
 static lmh_callback_t lora_callbacks = {BoardGetBatteryLevel, BoardGetUniqueId, BoardGetRandomSeed,
-										lorawan_rx_handler, lorawan_has_joined_handler, lorawan_confirm_class_handler, lorawan_join_failed_handler};//Structure containing LoRaWan callback functions, needed for lmh_init()
+  									lorawan_rx_handler, lorawan_has_joined_handler, lorawan_confirm_class_handler, lorawan_join_failed_handler};//Structure containing LoRaWan callback functions, needed for lmh_init()
 
 uint8_t nodeDeviceEUI[8], nodeAppEUI[8];
 uint8_t nodeAppKey[16], nodeNwsKey[16], nodeAppsKey[16];
@@ -51,7 +62,7 @@ void notFound(AsyncWebServerRequest *request) {
 }
 
 TinyGPSPlus gps;
-TinyGPSCustom gpsFix(gps, "GPGGA", 5);
+TinyGPSCustom gpsFix(gps, "GPGGA", 6);
 
 LSM6DS3 myIMU;
 
@@ -67,8 +78,10 @@ unsigned long gpsStatMillis = millis();
 float deadzoneX=0,deadzoneY=0,deadzoneZ=0;
 float detectVal=0.4;
 bool doDrag = false, gpsReady = false;
+
 int opMode = 0; //0 - normal, 1 - drag
 int currentRideID = 0;
+bool enaLora = true;
 
 bool loadSPIFFS() {
   DynamicJsonDocument doc(512);
@@ -84,11 +97,13 @@ bool loadSPIFFS() {
   DeserializationError error = deserializeJson(doc, file);
   if (error){
     Serial.println("Failed to deserialize the config file!");
+    file.close();
     return false;
   }
   file.close();
 
   currentRideID = doc["currentRideID"].as<int>();
+  enaLora = doc["enaLora"].as<bool>();
   return true;
 }
 
@@ -104,17 +119,17 @@ bool saveSPIFFS() {
   Serial.println("Config file opened!");
 
   doc["currentRideID"] = currentRideID;
+  doc["enaLora"] = enaLora;
 
   if (serializeJson(doc, file) == 0) {
     Serial.println("Failed to deserialize the config file");
+    file.close();
     return false;
   }
   file.close();
 
   return true;
 }
-
-bool gpspause = false;
 
 void setup() {
   for(int x = 0;x<numOfSpeeds;x++) {
@@ -128,20 +143,32 @@ void setup() {
   delay(1000);
   Serial.println("Processor came out of reset.\n");
 
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+  }
+
+  pinMode(SD_CS, OUTPUT);
+  SPISD.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+  if(!SD.begin(SD_CS,SPISD)){
+      Serial.println("Card Mount Failed");
+  } else {
+    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  }
+
   myIMU.begin();
 
   Serial2.begin(115200, SERIAL_8N1, 16, 17); //RX 16, TX 17
 
   pinMode(0, INPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(LORALED, OUTPUT);
+  digitalWrite(LORALED, LOW);
 
   WiFi.softAP(ssid, password);
   Serial.println(WiFi.softAPIP());
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     String sout = "<a href='/drag'>DRAG</a><br><a href='/normal'>NORMAL</a>";
-    request->send(200, "text/plain", sout);
+    request->send(200, "text/html", sout);
   });
   server.on("/drag", HTTP_GET, [](AsyncWebServerRequest *request){
     modeSwitch(1);
@@ -151,6 +178,26 @@ void setup() {
     modeSwitch(0);
     request->send(200, "text/plain", "ok");
   });
+  server.on("/getfile", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(request->hasParam("rideid")) {
+      String path = "/"+request->getParam("rideid")->value()+".data";
+      Serial.printf("Reading file: %s\n", path);
+      File file = SD.open(path);
+      if(!file){
+          Serial.println("Failed to open file for reading");
+          return;
+      }
+      String dataFileContents;
+      while(file.available()){
+          dataFileContents += (char)file.read();
+      }
+      //Serial.println(dataFileContents);
+      file.close();
+      request->send(200, "text/plain", dataFileContents);
+    } else {
+      request->send(200, "text/plain", "Parameter 'rideid' not present!");
+    }
+  });
 
   server.onNotFound(notFound);
   server.begin();
@@ -158,9 +205,10 @@ void setup() {
   calibrateAcc();
   loadSPIFFS();
 
-  copyArrays();
-  gpspause = true;
-  LoraSetup();
+  if(enaLora) {
+    copyArrays();
+    LoraSetup();
+  }
 }
 
 unsigned long detectMoveMillis = millis(), lastLora = millis(), lastNotMovedLora = millis();
@@ -170,10 +218,10 @@ double lastLat = 48.85826 ,lastLng = 2.294516, startLat = 0.0, startLng = 0.0;
 
 void loop()
 {
-  while (Serial2.available() > 0 && !gpspause) {
+  while (Serial2.available() > 0) {
     if (gps.encode(Serial2.read())) {
       if(millis()>(gpsStatMillis+5000)) {
-        if(gps.location.age() > 2000) {// || gpsFix.value() == 0 || gps.satellites.value() < 3
+        if(gps.location.age() > 2000 || gpsFix.value() == 0 || gps.satellites.value() < 2) {
           gpsReady = false;
         } else {
           gpsReady = true;
@@ -183,11 +231,9 @@ void loop()
     }
   }
 
-  if(gpsReady==true && routeActive==false && millis()>lastNotMovedLora+60000 /*&& loraReady*/){ //předělat na delší čas,
-    gpspause = true;
+  if(gpsReady==true && routeActive==false && millis()>lastNotMovedLora+60000){ //předělat na delší čas,
+    Serial.println("sending data");
     sendPos("n");
-    gpspause = false;
-    Serial.println("sending lora");
     lastNotMovedLora = millis();
   }
 
@@ -254,30 +300,54 @@ void loop()
     }
 
     if(doDrag == true) {
-       if((myIMU.readFloatAccelX()-deadzoneX)>detectVal || (myIMU.readFloatAccelY()-deadzoneY)>detectVal || (myIMU.readFloatAccelZ()-deadzoneZ)>detectVal) {
+      if((myIMU.readFloatAccelX()-deadzoneX)>detectVal || (myIMU.readFloatAccelY()-deadzoneY)>detectVal || (myIMU.readFloatAccelZ()-deadzoneZ)>detectVal) {
         Serial.println("Movement detected on X");
         doDrag = false;
         startLat = gps.location.lat();
         startLng = gps.location.lng();
         dragTimer();
-       }
+      }
     }
   }
 }
 
 uint32_t getUnix(uint32_t year, uint32_t month, uint32_t day, uint32_t hour, uint32_t minute, uint32_t second) {
-        int8_t my = (month >= 3) ? 1 : 0;
-        uint16_t y = year + my - 1970;
-        uint16_t dm = 0;
-        for (int i = 0; i < month - 1; i++) dm += (i<7)?((i==1)?28:((i&1)?30:31)):((i&1)?31:30);
-        return (((day-1+dm+((y+1)>>2)-((y+69)/100)+((y+369)/100/4)+365*(y-my))*24ul+hour)*60ul+minute)*60ul+second;
-    }
+  int8_t my = (month >= 3) ? 1 : 0;
+  uint16_t y = year + my - 1970;
+  uint16_t dm = 0;
+  for (int i = 0; i < month - 1; i++) dm += (i<7)?((i==1)?28:((i&1)?30:31)):((i&1)?31:30);
+  return (((day-1+dm+((y+1)>>2)-((y+69)/100)+((y+369)/100/4)+365*(y-my))*24ul+hour)*60ul+minute)*60ul+second;
+}
 
 bool sendPos(String moving) { //rideid;moving;lat;lng;kmph;epoch - opravdu celý float?
   String out = String(currentRideID)+";"+moving+";"+String(gps.location.lat(),5)+";"+String(gps.location.lng(),5)+";"+String(gps.speed.kmph())+";"+String(getUnix(gps.date.year(),gps.date.month(),gps.date.day(),gps.time.hour(),gps.time.minute(),gps.time.second()));
   Serial.println(out);
-  //write to file?
-  send_lora_frame(out);
+  fileOper(SD,"/"+String(currentRideID)+".data",out);
+  if(enaLora) { /*&& loraReady*/
+    send_lora_frame(out);
+  }
+}
+
+void fileOper(fs::FS &fs, String path, String message) {
+  String msg;
+  File file;
+  if(fs.exists(path)) {
+    file = fs.open(path, FILE_APPEND);
+    msg = "append";
+  } else {
+    file = fs.open(path, FILE_WRITE);
+    msg = "write";
+  }
+  if(!file){
+    Serial.println("Failed to open file for "+msg);
+    return;
+  }
+  if(file.println(message)){
+    Serial.println("Data "+msg);
+  } else {
+    Serial.println("Failed data "+msg);
+  }
+  file.close();
 }
 
 void modeSwitch(int opm) {
@@ -353,69 +423,67 @@ int dragTimer() { //this needs to run on another core
 
 void LoraSetup() {
   // Define the HW configuration between MCU and SX126x
-	hwConfig.CHIP_TYPE = SX1262_CHIP;		  // Example uses an eByte E22 module with an SX1262
-	hwConfig.PIN_LORA_RESET = 4;    // LORA RESET
-	hwConfig.PIN_LORA_NSS = 5;	    // LORA SPI CS
-	hwConfig.PIN_LORA_SCLK = 18;	  // LORA SPI CLK
-	hwConfig.PIN_LORA_MISO = 19;	  // LORA SPI MISO
-	hwConfig.PIN_LORA_DIO_1 = 2;    // LORA DIO_1
-	hwConfig.PIN_LORA_BUSY = 34;	  // LORA SPI BUSY
-	hwConfig.PIN_LORA_MOSI = 23;	  // LORA SPI MOSI
-	hwConfig.RADIO_TXEN = -1;		  // LORA ANTENNA TX ENABLE
-	hwConfig.RADIO_RXEN = -1;		  // LORA ANTENNA RX ENABLE
-	hwConfig.USE_DIO2_ANT_SWITCH = true;	  // Example uses an CircuitRocks Alora RFM1262 which uses DIO2 pins as antenna control
-	hwConfig.USE_DIO3_TCXO = false;			  // Example uses an CircuitRocks Alora RFM1262 which uses DIO3 to control oscillator voltage
-	hwConfig.USE_DIO3_ANT_SWITCH = false;	  // Only Insight ISP4520 module uses DIO3 as antenna control
+  hwConfig.CHIP_TYPE = SX1262_CHIP;		  // Example uses an eByte E22 module with an SX1262
+  hwConfig.PIN_LORA_RESET = 4;    // LORA RESET
+  hwConfig.PIN_LORA_NSS = 5;	    // LORA SPI CS
+  hwConfig.PIN_LORA_SCLK = 18;	  // LORA SPI CLK
+  hwConfig.PIN_LORA_MISO = 19;	  // LORA SPI MISO
+  hwConfig.PIN_LORA_DIO_1 = 2;    // LORA DIO_1
+  hwConfig.PIN_LORA_BUSY = 34;	  // LORA SPI BUSY
+  hwConfig.PIN_LORA_MOSI = 23;	  // LORA SPI MOSI
+  hwConfig.RADIO_TXEN = -1;		  // LORA ANTENNA TX ENABLE
+  hwConfig.RADIO_RXEN = -1;		  // LORA ANTENNA RX ENABLE
+  hwConfig.USE_DIO2_ANT_SWITCH = true;	  // Example uses an CircuitRocks Alora RFM1262 which uses DIO2 pins as antenna control
+  hwConfig.USE_DIO3_TCXO = false;			  // Example uses an CircuitRocks Alora RFM1262 which uses DIO3 to control oscillator voltage
+  hwConfig.USE_DIO3_ANT_SWITCH = false;	  // Only Insight ISP4520 module uses DIO3 as antenna control
   hwConfig.USE_RXEN_ANT_PWR = false;
 
-	// Initialize Serial for debug output
-	Serial.begin(115200);
-	Serial.println("SX126x LoRaWan");
+  // Initialize Serial for debug output
+  Serial.println("SX126x LoRaWan");
 
-	// Initialize LoRa chip.
-	uint32_t err_code = lora_hardware_init(hwConfig);
-	if (err_code != 0)
-	{
-		Serial.printf("lora_hardware_init failed - %d\n", err_code);
-	}
+  // Initialize LoRa chip.
+  uint32_t err_code = lora_hardware_init(hwConfig);
+  if (err_code != 0)
+  {
+  	Serial.printf("lora_hardware_init failed - %d\n", err_code);
+  }
 
-	// Setup the EUIs and Keys
-	lmh_setDevEui(nodeDeviceEUI);
-	lmh_setAppEui(nodeAppEUI);
-	lmh_setAppKey(nodeAppKey);
-	lmh_setNwkSKey(nodeNwsKey);
-	lmh_setAppSKey(nodeAppsKey);
-	lmh_setDevAddr(nodeDevAddr);
+  // Setup the EUIs and Keys
+  lmh_setDevEui(nodeDeviceEUI);
+  lmh_setAppEui(nodeAppEUI);
+  lmh_setAppKey(nodeAppKey);
+  lmh_setNwkSKey(nodeNwsKey);
+  lmh_setAppSKey(nodeAppsKey);
+  lmh_setDevAddr(nodeDevAddr);
 
-	// Initialize LoRaWan
-	err_code = lmh_init(&lora_callbacks, lora_param_init, true, CLASS_A, LORAMAC_REGION_EU868);
-	if (err_code != 0)
-	{
-		Serial.printf("lmh_init failed - %d\n", err_code);
-	}
+  // Initialize LoRaWan
+  err_code = lmh_init(&lora_callbacks, lora_param_init, true, CLASS_A, LORAMAC_REGION_EU868);
+  if (err_code != 0)
+  {
+  	Serial.printf("lmh_init failed - %d\n", err_code);
+  }
 
-	// Setup connection to a single channel gateway
+  // Setup connection to a single channel gateway
   //lmh_setSingleChannelGateway(2, DR_3);
 
-	// For some regions we might need to define the sub band the gateway is listening to
-	// This must be called AFTER lmh_init()
-	/// \todo This is for Dragino LPS8 gateway. How about other gateways???
-	if (!lmh_setSubBandChannels(1))
-	{
-		Serial.println("lmh_setSubBandChannels failed. Wrong sub band requested?");
-	}
+  // For some regions we might need to define the sub band the gateway is listening to
+  // This must be called AFTER lmh_init()
+  /// \todo This is for Dragino LPS8 gateway. How about other gateways???
+  if (!lmh_setSubBandChannels(1))
+  {
+  	Serial.println("lmh_setSubBandChannels failed. Wrong sub band requested?");
+  }
 
-	// Start Join procedure
-	lmh_join();
+  // Start Join procedure
+  lmh_join();
 }
 
 //LoRa function for handling OTAA join failed
 static void lorawan_join_failed_handler(void) {
-	Serial.println("OVER_THE_AIR_ACTIVATION failed!");
-	Serial.println("Check your EUI's and Keys's!");
-	Serial.println("Check if a Gateway is in range!");
-  gpspause = false;
-  digitalWrite(13, LOW);
+  Serial.println("OVER_THE_AIR_ACTIVATION failed!");
+  Serial.println("Check your EUI's and Keys's!");
+  Serial.println("Check if a Gateway is in range!");
+  digitalWrite(LORALED, LOW);
 }
 
 //LoRa function for handling HasJoined event.
@@ -429,71 +497,70 @@ static void lorawan_has_joined_handler(void) {
   lmh_class_request(CLASS_A);
 
   //lora ready (what about not ready? disconnected..)
-  gpspause = false;
-  digitalWrite(13, HIGH);
+  digitalWrite(LORALED, HIGH);
 }
 
 //Function for handling LoRaWan received data from Gateway
 static void lorawan_rx_handler(lmh_app_data_t *app_data) { //app_data - Pointer to rx data
-	Serial.printf("LoRa Packet received on port %d, size:%d, rssi:%d, snr:%d\n",
-				  app_data->port, app_data->buffsize, app_data->rssi, app_data->snr);
+  Serial.printf("LoRa Packet received on port %d, size:%d, rssi:%d, snr:%d\n",
+  			  app_data->port, app_data->buffsize, app_data->rssi, app_data->snr);
 
-	switch (app_data->port)
-	{
-	case 3:// Port 3 switches the class
-		if (app_data->buffsize == 1)
-		{
-			switch (app_data->buffer[0])
-			{
-			case 0:
-				lmh_class_request(CLASS_A);
-				break;
+  switch (app_data->port)
+  {
+  case 3:// Port 3 switches the class
+  	if (app_data->buffsize == 1)
+  	{
+  		switch (app_data->buffer[0])
+  		{
+  		case 0:
+  			lmh_class_request(CLASS_A);
+  			break;
 
-			case 1:
-				lmh_class_request(CLASS_B);
-				break;
+  		case 1:
+  			lmh_class_request(CLASS_B);
+  			break;
 
-			case 2:
-				lmh_class_request(CLASS_C);
-				break;
+  		case 2:
+  			lmh_class_request(CLASS_C);
+  			break;
 
-			default:
-				break;
-			}
-		}
-		break;
+  		default:
+  			break;
+  		}
+  	}
+  	break;
 
-	case LORAWAN_APP_PORT:
-		// YOUR_JOB: Take action on received data
-		break;
+  case LORAWAN_APP_PORT:
+  	// YOUR_JOB: Take action on received data
+  	break;
 
-	default:
-		break;
-	}
+  default:
+  	break;
+  }
 }
 
 static void lorawan_confirm_class_handler(DeviceClass_t Class) {
-	Serial.printf("switch to class %c done\n", "ABC"[Class]);
+  Serial.printf("switch to class %c done\n", "ABC"[Class]);
 
-	// Informs the server that switch has occurred ASAP
-	m_lora_app_data.buffsize = 0;
-	m_lora_app_data.port = LORAWAN_APP_PORT;
-	lmh_send(&m_lora_app_data, LMH_UNCONFIRMED_MSG);
+  // Informs the server that switch has occurred ASAP
+  m_lora_app_data.buffsize = 0;
+  m_lora_app_data.port = LORAWAN_APP_PORT;
+  lmh_send(&m_lora_app_data, LMH_UNCONFIRMED_MSG);
 }
 
 static void send_lora_frame(String toSend) {
-	if (lmh_join_status_get() != LMH_SET)
-	{
-		//Not joined, try again later
-		Serial.println("Did not join network, skip sending frame");
-		return;
-	}
+  if (lmh_join_status_get() != LMH_SET)
+  {
+  	//Not joined, try again later
+  	Serial.println("Did not join network, skip sending frame");
+  	return;
+  }
 
-	uint32_t i = 0;
-	m_lora_app_data.port = LORAWAN_APP_PORT;
+  uint32_t i = 0;
+  m_lora_app_data.port = LORAWAN_APP_PORT;
   toSend.toCharArray(reinterpret_cast<char *>(m_lora_app_data.buffer), toSend.length()+1);
-	m_lora_app_data.buffsize = toSend.length();
+  m_lora_app_data.buffsize = toSend.length();
 
-	lmh_error_status error = lmh_send(&m_lora_app_data, LMH_UNCONFIRMED_MSG);
-	Serial.printf("lmh_send result %d\n", error);
+  lmh_error_status error = lmh_send(&m_lora_app_data, LMH_UNCONFIRMED_MSG);
+  Serial.printf("lmh_send result %d\n", error);
 }
--- webhook-nodejs/main.js
+++ webhook-nodejs/main.js
@@ -12,22 +12,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.post('/uplink', function (req, res) {
-	var jsonData = JSON.stringify(req.body);
-	var jsonParsed = JSON.parse(jsonData);
+	var jsonParsed = JSON.parse(JSON.stringify(req.body));
 	var buf = Buffer.from(jsonParsed.uplink_message.frm_payload, 'base64');
 	var str = buf.toString('utf-8');
 	console.log(str);
 	const dataArray = str.split(";");
-	var out = {rideId:dataArray[0],moving:dataArray[1],lat:dataArray[2],lng:dataArray[3],speed:dataArray[4],epoch:dataArray[5]};
+	var out = {rideId:dataArray[0],moving:dataArray[1],lat:parseFloat(dataArray[2]),lng:parseFloat(dataArray[3]),speed:parseFloat(dataArray[4]),epoch:dataArray[5]};
   io.emit("loraDecoded", out.moving, out.lat, out.lng, out.speed, out.epoch);
 	saveUplink(out);
   res.status(200).end()
 });
 
 function search(array, insert, cb) {
-    var left = -1,
-        right = array.length,
-        actual;
+    var left = -1, right = array.length, actual;
 
     while (left !== right && left + 1 !== right) {
         actual = Math.floor((left + right) / 2);
@@ -49,31 +46,75 @@ function saveUplink(data) {
 		let insert = {"moving":data.moving,"lat":data.lat,"lng":data.lng,"speed":data.speed,"epoch":data.epoch};
 		var index = search(currentFileContent, insert, function (a) { return a.epoch; });
 		currentFileContent.splice(index + 1, 0, insert);
+		setTimeout(function(){calcStuff(data.rideId);}, 5000);
   } else { //create entry in db
 		let dbData = JSON.parse(fs.readFileSync("routes.json", 'utf8'));
-    dbData.push({"id": data.rideId,"startEpoch": 0,"max": 0,"avg": 0});
-		//!!!dodělat startEpoch,max,avg - calc function při každém přidání
-    jsonStr = JSON.stringify(dbData, null, 2);
-    fs.writeFile("routes.json", jsonStr, err => {
-      if (err) {
+    dbData.push({"id": data.rideId,"startEpoch": 0,"max": 0,"avg": 0,"total": 0});
+    fs.writeFile("routes.json", JSON.stringify(dbData, null, 2), err => {
+      if (err)
         console.log(`Data couldn't be saved! Error: ${err}`);
-      }
     });
 		let newjson = [];
 	  newjson.push({"moving":data.moving,"lat":data.lat,"lng":data.lng,"speed":data.speed,"epoch":data.epoch});
 	}
 	fs.writeFile("routes/"+String(data.rideId)+".json", String(JSON.stringify(currentFileContent, null, 2)), err => {
-    if (err) {
+    if (err)
       console.log(`Data couldn't be saved! Error: ${err}`);
-    }
   });
 }
 
+function calcStuff(rideId){
+	var rideData = JSON.parse(fs.readFileSync("routes/"+String(rideId)+".json", 'utf8'));
+	let maxSpeed = 0, avgSpeed = 0, avgCount = 0, totalKM = 0;
+	rideData.forEach(function(obj,index) {
+		if(obj.speed > maxSpeed) {
+			maxSpeed = obj.speed;
+		}
+		avgSpeed += obj.speed;
+		avgCount++;
+		if(index > 0) {
+			totalKM += getDistanceFromCoords(obj.lat,obj.lng,rideData[index-1].lat,rideData[index-1].lng);
+		}
+	});
+	avgSpeed = avgSpeed/avgCount;
+	let dbData = JSON.parse(fs.readFileSync("routes.json", 'utf8'));
+	dbData.forEach(function(obj,index) {
+		if(obj.id == rideId) {
+			dbData[index].total = parseFloat(totalKM.toFixed(2));
+			dbData[index].max = parseFloat(maxSpeed.toFixed(2));
+			dbData[index].avg = parseFloat(avgSpeed.toFixed(2));
+			dbData[index].startEpoch = rideData[0].epoch;
+		}
+	});
+	fs.writeFile("routes.json", JSON.stringify(dbData, null, 2), err => {
+		if (err)
+			console.log(`Data couldn't be saved! Error: ${err}`);
+	});
+}
+
+function getDistanceFromCoords(lat1, lon1, lat2, lon2) {
+  var dLat = deg2rad(lat2-lat1);
+  var dLon = deg2rad(lon2-lon1);
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+					Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+			    Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = 6371 * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
+
 io.on('connection', (socket) => {
+	socket.on('reCalc', rideId => { //signal to recalculate rides
+    calcStuff(rideId);
+  });
   socket.on('getRoutes', function() { //signal to send routes from json file
     let data = JSON.parse(fs.readFileSync("routes.json", 'utf8'));
     data.forEach(function(obj) {
-      socket.emit('loadRoutes', obj.id, obj.startEpoch, obj.max, obj.avg);
+      socket.emit('loadRoutes', obj.id, obj.startEpoch, obj.max, obj.avg, obj.total);
     });
   });
   socket.on('getPoints', routeId => { //signal to send points from specific ride
@@ -91,8 +132,8 @@ io.on('connection', (socket) => {
 });
 
 process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-	socket.emit('error', 'Caught exception: ' + err);
+	io.emit('error', 'Caught exception: ' + err.stack);
+	console.error(err.stack || err);
 });
 
 const PORT = process.env.PORT || 80;
